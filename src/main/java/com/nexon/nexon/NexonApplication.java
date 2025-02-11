0a979fb65a763d9eee2fd1c8eba25a6808e182c9
nicolasm9808 <nicolas-mendez@hotmail.com>
2025-02-11 17:06:37 -0500

    build: :tada: Spring Boot setup

---
+++ src/main/java/com/nexon/nexon/NexonApplication.java
@@ -0,0 +1,13 @@
+package com.nexon.nexon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexonApplication.class, args);
+	}
+
+}
