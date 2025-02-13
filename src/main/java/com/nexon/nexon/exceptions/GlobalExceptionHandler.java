222fc3d8c478bdf62d072c96566eb3e21007307c
nicolasm9808 <nicolas-mendez@hotmail.com>
2025-02-13 12:34:01 -0500

    feat: :sparkles: Implemented Global Exceptions

---
+++ src/main/java/com/nexon/nexon/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.nexon.nexon.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred: " + ex.getMessage());
+    }
+}
